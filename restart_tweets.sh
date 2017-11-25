9b175f74a8f91c9c7a7cc7b23e8a5b5464609379
diskerror <reidw@rawsound.com>
2017-11-24 16:16:19 -0800

    removed run

--- restart_tweets.sh
+++ restart_tweets.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-/var/www/html/run stop tweets
+/var/www/html/cli.php stop tweets
 sleep 1
 if [ ! -e /var/run/harvest ]
 then
     mkdir -pm 777 /var/run/harvest
 fi
-/var/www/html/run get tweets &
+/var/www/html/cli.php get tweets &
