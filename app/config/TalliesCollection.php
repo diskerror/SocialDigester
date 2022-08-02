48e3c86f2d0fad4a10e3d98bcb8938fffb6e80a2
Reid Woodbury Jr <reid@rawsound.com>
2022-08-02 15:59:42 -0700

    Fixed indexing typo.

--- app/config/TalliesCollection.php
+++ app/config/TalliesCollection.php
@@ -7,6 +7,6 @@ return new CollectionDefinition([
 	'name'      => 'tallies',
 	'class'     => Tally::class,
 	'indexKeys' => [
-		['key' => ['created' => 1], 'expireAfterSeconds' => 600],
+		['key' => ['created' => 1], 'expireAfterSeconds' => 3600],
 	],
 ]);
--- app/config/TweetsCollection.php
+++ app/config/TweetsCollection.php
@@ -7,7 +7,7 @@ return new CollectionDefinition([
 	'name'      => 'tweets',
 	'class'     => Tweet::class,
 	'indexKeys' => [
-		['key' => ['created_at' => 1], 'expireAfterSeconds' => 3600],
+		['key' => ['created_at' => 1], 'expireAfterSeconds' => 900],
 		['key' => ['entities . hashtags.0 . text' => 1]],
 	],
 ]);
